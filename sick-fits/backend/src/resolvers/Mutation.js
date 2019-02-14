7b6afa5096f318b55eea9b2c0a4d906c2574c203
Ben Coleman <benjamin.coleman@uconn.edu>
2019-02-14 18:39:06 -0500

    finish 19

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,27 +1,36 @@
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        // TODO: Check if they are logged in
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
 
-        const item = await ctx.db.mutation.createItem(
-            {
-                data: {
-                    ...args,
-                },
-            },
-            info
-        );
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-        console.log(item);
+    console.log(item);
 
-        return item;
-    },
-    // createDog(parent, args, ctx, info) {
-    //   global.dogs = global.dogs || [];
-    //   // create a dog
-    //   const newDog = { name: args.name };
-    //   global.dogs.push(newDog);
-    //   return newDog;
-    // },
+    return item;
+  },
+  updateItem(parents, args, ctx, info) {
+    // first take copy of the updates
+    const updates = { ...args };
+    // remove id from updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Mutations;
